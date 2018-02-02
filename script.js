941c73654cf0c9ccca1db59665f55835068b7f71
Kalen Hammann <kalen7@gmail.com>
2018-02-02 18:08:57 -0500

    Stop the timer and make the Start Over button work

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 var timer = [0, 0, 0, 0];
+var interval;
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -31,6 +33,7 @@ function spellCheck() {
     let originTextMatch = originText.substring(0, textEntered.length);
 
     if (textEntered == originText) {
+        clearInterval(interval);
         testWrapper.style.borderColor = "#429890";
     } else {
         if (textEntered == originTextMatch) {
@@ -44,15 +47,23 @@ function spellCheck() {
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
-    if (textEnteredLength ===0 ) {
-        setInterval(runTimer, 10);
+    if (textEnteredLength === 0 && !timerRunning ) {
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
     }
     console.log(textEnteredLength);
 }
 
 // Reset everything:
 function reset() {
-    console.log("Reset button has been pressed!");
+    clearInterval(interval);
+    interval = null;
+    timer = [0,0,0,0];
+    timerRunning = false
+
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
 }
 
 // Event listeners for keyboard input and the reset button:
